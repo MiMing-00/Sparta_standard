b106190baf927fad106ab81a45d05f8c5399c6e7
Sera Choi <c.sera2511@gmail.com>
2024-05-17 19:46:45 +0900

    reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 import "./App.css";
-import Layout from "../component/Layout";
-import ToDoContain from "../component/ToDoContain";
 
 function App() {
   return (
-    <Layout>
-      <ToDoContain />
-    </Layout>
+
   );
 }
 
