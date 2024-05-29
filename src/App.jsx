4f822748da9f3a627bb3b4ae7d6ebbb6781d05a5
Sera Choi <c.sera2511@gmail.com>
2024-05-29 21:03:30 +0900

    ex4 done/ customhook

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import useFetch from "./hooks/useFetch";
+
+// TODO: useFetch 라는 커스텀훅을 사용해서 리팩터링 해보세요
+
+const App = () => {
+  const { title, loading, error } = useFetch();
+
+  if (loading) return <h1>Loading...</h1>;
+  if (error) return <h1>Error: {error.message}</h1>;
+
+  return (
+    <div>
+      <h1>Data Fetching Example</h1>
+      <p>{title}</p>
+    </div>
+  );
+};
+
+export default App;
